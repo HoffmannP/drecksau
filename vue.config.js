c3568771d086dbb7c4f9294821752e6f85ec5b5e
Berengar W. Lehr <Berengar.Lehr@kompetenztest.de>
2019-07-03 17:02:48 +0200

    Fix: Nicht weiter gekommen

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
   productionSourceMap: false,
   outputDir: 'docs',
-  publicPath: '/drecksau/',
+  ...(process.env.NODE_ENV === 'production'
+    ? { publicPath: '/drecksau/' }
+    : {}
+  )
 }
